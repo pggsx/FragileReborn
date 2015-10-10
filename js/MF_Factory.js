04819069cb44f259e456cb4a71b65459c019f634
Pavan Gudimetta <pgudimetta@gmail.com>
2015-10-10 00:11:22 -0400

    barebones

---
+++ js/MF_Factory.js
@@ -0,0 +1,20 @@
+function MixedFraction(num,denom,whole_num)
+{
+	this.num = num;
+	this.denom = denom;
+	this.whole_num = whole_num;	
+}
+
+var MF_Factory[];
+
+function fract_store(MixedFraction fract)
+{
+	MF_Factory.push(fract);
+}
+
+function fract_calc(MF_Factory[] fract_list)
+{
+
+
+
+}
