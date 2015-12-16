593410b1446e0aaf1b26b09b3350b6fbe7470639
Pavan Gudimetta <pgudimetta@gmail.com>
2015-12-15 23:56:15 -0500

    updated with getters and setters

--- js/MF_Factory.js
+++ js/MF_Factory.js
@@ -2,14 +2,31 @@
  *
  */
 var MF_Factory[];
-
+/**
+ *
+ */
 function fract_store(MixedFraction fract)
 {
 	MF_Factory.push(fract);
 }
-
-function fract_calc(MF_Factory[] fract_list)
+/* fract_calc
+ * params: MF_Factory[] fract_list
+ * calculates Mixed Fractions Objects total sum and returns sum
+ */
+function MixedFraction fract_calc(MF_Factory[] fract_list)
 {
+	if(fract_list != undefined)
+		{
+		MixedFraction sum;
+			var x;
+			for(x in fract_list)
+				{
 
-
+				}
+		}
+	else
+		{
+			alert("Invalid List of Fractions");
+		}
+	return sum;
 }
--- js/MixedFraction.js
+++ js/MixedFraction.js
@@ -12,22 +12,12 @@ function MixedFraction(numer,denom,whole_num)
 	this.whole_num = whole_num;	
 }
 
-function getDenom()
-{
-	return this.denom;
-}
-/***
- *
- *
- */
-function getNumer()
-{
-	return this.numer;
-}
+
 
 /*
- *
- *
+ * setDenom
+ * params: new_denom
+ * setter method that sets the denominator of MixedFraction Object
  */
 function setDenom(int new_denom)
 {
@@ -35,8 +25,9 @@ function setDenom(int new_denom)
 }
 
 /*
- *
- *
+ * setNumer
+ * param: new_numer
+ * setter method that sets the numerator of MixedFraction Object
  */
 function setNumer(int new_numer)
 {
@@ -44,8 +35,9 @@ function setNumer(int new_numer)
 }
 
 /*
- *
- *
+ * setWholeNum
+ * param: new_whole_num
+ * setter methods that sets the whole number of MixedFraction Object
  */
 function setWholeNum(int new_whole_num)
 {
@@ -53,12 +45,39 @@ function setWholeNum(int new_whole_num)
 }
 
 
+/***
+ *
+ * getter method that returns the numerator
+ */
+function getDenom()
+{
+	return this.denom;
+}
+/***
+ *
+ * getter method that returns the numerator
+ */
+function getNumer()
+{
+	return this.numer;
+}
+
+
+/***
+ *
+ * getter method that returns the whole number
+ */
+function getWholeNum()
+{
+	return this.whole_num;
+}
 /*
- *
- *
+ * ToString Function 
+ * Returns String Representation of Mixed Fraction Object;
  */
 function String toString()
 {
 	String fract_obj;
 	fract_obj = this.whole_num + this.numer + "/" + this.denom;
+	return fract_obj;
 }
